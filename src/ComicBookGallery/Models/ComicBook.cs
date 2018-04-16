db7ad4d2d66ba250590abf84a2156c193a3cb35c
Hemory Phifer <hemoryphifer@quickenloans.com>
2018-04-16 16:14:37 -0400

    Added comic book arist and models

---
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }             
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public String DescriptionHtml { get; set; }
+        public Artist[] Artist { get; set; }
+        public bool IsFavorite { get; set; }
+    }
+}
