f7e85f05b44ac34470519c45c28d1e4d07f7f19c
Hemory Phifer <hemoryphifer@quickenloans.com>
2018-04-25 11:24:29 -0400

    Updated the Add action method to capture the post data

--- src/fitness-frog/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/fitness-frog/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using Treehouse.FitnessFrog.Data;
 using Treehouse.FitnessFrog.Models;
@@ -45,8 +43,15 @@ namespace Treehouse.FitnessFrog.Controllers
         }
         [ActionName("Add")]
         [HttpPost]
-        public ActionResult AddPost()
+        public ActionResult Add(string date, string activityId, string duration, string intensity, string exclude, string notes)
         {
+            ViewBag.Date = date;
+            ViewBag.ActivityId = activityId;
+            ViewBag.Duration = duration;
+            ViewBag.Intensity = intensity;
+            ViewBag.Exclude = exclude;
+            ViewBag.Notes = notes;
+
             return View();
         }
 
